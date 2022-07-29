4b7530fe53e2ec08a855610d0f164d568057d317
Max <max.wang@tpisoftware.com>
2022-07-29 15:37:35 +0800

    Add Category/Create Controller

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,10 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
