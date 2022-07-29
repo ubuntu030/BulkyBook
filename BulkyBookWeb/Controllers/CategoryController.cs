188518566629212eb9a0418ee99dd73560871c21
Max <max.wang@tpisoftware.com>
2022-07-29 16:50:59 +0800

    Server-side Customer validation

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,6 +30,11 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                // [TryAddModelError] keyParam: 可制定或對應到 CategoryModel 的 Key Name
+                ModelState.TryAddModelError("Name", "The DisplayOrder cannot exactly match the Name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
